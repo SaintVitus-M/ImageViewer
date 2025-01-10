6c5236b5b797d84d4a8b184d0d98e7ccec71f998
Vit <vit@email.com>
2025-01-10 20:25:36 +0000

    Main App implementation: 1. Creates the image display into the main frame. 2. Creates the image presenter with the display. 3. Creates image loader. 4. Sets the main frame visible, showing the first image from the list.

--- src/main/java/software/ulpgc/images/Main.java
+++ src/main/java/software/ulpgc/images/Main.java
@@ -1,7 +1,19 @@
 package software.ulpgc.images;
 
+import software.ulpgc.images.architecture.io.ImageLoader;
+import software.ulpgc.images.picsum.PicsumImagePresenter;
+import software.ulpgc.images.picsum.PicsumImageLoader;
+import software.ulpgc.images.swing.SwingImageDisplay;
+import software.ulpgc.images.swing.SwingMainFrame;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main() {
+
+        SwingImageDisplay imageDisplay = new SwingImageDisplay();
+        SwingMainFrame mainFrame = new SwingMainFrame(imageDisplay);
+        PicsumImagePresenter presenter = new PicsumImagePresenter(imageDisplay);
+        ImageLoader loader = new PicsumImageLoader();
+        presenter.showWith(loader);
+        mainFrame.setVisible(true);
     }
 }
